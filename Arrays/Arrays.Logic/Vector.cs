962977eb610cb16eb939c7f9bae077ae0dc355f6
Gerardo Lanza <glanza007@gmail.com>
2023-03-10 16:10:43 -0600

    insert,remove,add and odds

--- Arrays/Arrays.Logic/Vector.cs
+++ Arrays/Arrays.Logic/Vector.cs
@@ -16,6 +16,87 @@
         public bool IsEmpty => _top == 0;
         public int N { get; }//tamano fisico
 
+        public void Add(int number) 
+        { 
+            if(IsFull)
+            {
+                throw new Exception("El arreglo esta lleno");
+            }
+            _array[_top] = number;
+            _top++;           
+        }
+
+        public void Insert(int number, int position)
+        {
+            if(IsFull) 
+            {
+                throw new Exception("El arreglo esta lleno");
+            }
+
+            if(position < 0)
+            {
+                position = 0;
+            }
+
+            if(position > _top)
+            {
+                position = _top;
+            }
+
+            for (int i = _top; i > position; i--)
+            {
+                _array[i] = _array[i - 1];
+            }
+            _array[position] = number;
+            _top++;
+
+        }
+
+        public void Remove(int position)
+        {
+            if(IsEmpty) 
+            {
+                throw new Exception("El arreglo esta vacio");
+            }
+            
+            if (position < 0)
+            {
+                position = 0;
+            }
+
+            if (position > _top)
+            {
+                position = _top;
+            }
+
+            for (int i = position; i < _top - 1; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+            _top--;
+        }
+
+        public Vector GetOdds() 
+        {
+            int oddsCount = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                if (_array[i]%2 == 0)
+                {
+                    oddsCount++;
+                }
+            }
+            var oddsArray = new Vector(oddsCount);
+            for (int i = 0; i < _top; i++)
+            {
+                if (_array[i]%2 == 0)
+                {
+                    oddsArray.Add(_array[i]);
+                }
+            }
+            return oddsArray;
+        }
+
         public void Fill(int min, int max )
         {
             Random random = new();
--- Arrays/Arrays.UI.Console/Program.cs
+++ Arrays/Arrays.UI.Console/Program.cs
@@ -2,14 +2,40 @@
 
 using Arrays.Logic;
 
-Console.WriteLine("Hello Vector!");
+try
+{
+	Console.WriteLine("Hello Vector!");
 
-Vector vector = new(10);
-vector.Fill(1, 100);
+	Vector vector = new(50);
+    vector.Fill(1, 100);
+    //vector.Add(10);
+    //vector.Add(20);
+    //vector.Add(30);
+    //vector.Insert(7, 0);
+    //for (int i = 0; i < vector.N; i++)
+    //{
+    //	vector.Add(i * 3);
+    //}
+    Console.WriteLine(vector);
+	//vector.Remove(0);
+	Console.WriteLine("------------------------");
+    var odds = vector.GetOdds();
+	Console.WriteLine(odds);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
-Console.WriteLine("Unsorted Array!");
-Console.WriteLine(vector);
+//vector.Fill(1, 100);
 
-vector.Sort(true);
-Console.WriteLine("Sorted Array!");
-Console.WriteLine(vector);
+//Console.WriteLine("Unsorted Array!");
+//Console.WriteLine(vector);
+
+//vector.Sort(true);
+//Console.WriteLine("Ascending Sorted Array!");
+//Console.WriteLine(vector);
+
+//vector.Sort(false);
+//Console.WriteLine("Descending Sorted Array!");
+//Console.WriteLine(vector);
