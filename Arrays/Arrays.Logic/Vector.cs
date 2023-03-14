964f78435e94894fcf2c96860ed949cf251bdba7
Gerardo Lanza <glanza007@gmail.com>
2023-03-14 11:13:50 -0600

    arrays full

--- Arrays/Arrays.Logic/Vector.cs
+++ Arrays/Arrays.Logic/Vector.cs
@@ -2,7 +2,7 @@
 {
     public class Vector
     {
-        private int _top;//tamano logico
+        private int _top; // Logic size
         private int[] _array;
 
         public Vector(int n)
@@ -12,33 +12,45 @@
             _top = 0;
         }
 
+        /// <summary>
+        /// Indicates if the array is full
+        /// </summary>
         public bool IsFull => _top == N;
-        public bool IsEmpty => _top == 0;
-        public int N { get; }//tamano fisico
 
-        public void Add(int number) 
-        { 
-            if(IsFull)
+        /// <summary>
+        /// Indicates if the array is empty
+        /// </summary>
+        public bool IsEmpty => _top == 0;
+
+        /// <summary>
+        /// Physical size of the array
+        /// </summary>
+        public int N { get; } // Physical size
+
+        public void Add(int number)
+        {
+            if (IsFull)
             {
-                throw new Exception("El arreglo esta lleno");
+                throw new Exception("The array is full.");
             }
+
             _array[_top] = number;
-            _top++;           
+            _top++;
         }
 
         public void Insert(int number, int position)
         {
-            if(IsFull) 
+            if (IsFull)
             {
-                throw new Exception("El arreglo esta lleno");
+                throw new Exception("The array is full.");
             }
 
-            if(position < 0)
+            if (position < 0)
             {
                 position = 0;
             }
 
-            if(position > _top)
+            if (position > _top)
             {
                 position = _top;
             }
@@ -47,18 +59,18 @@
             {
                 _array[i] = _array[i - 1];
             }
+
             _array[position] = number;
             _top++;
-
         }
 
         public void Remove(int position)
         {
-            if(IsEmpty) 
+            if (IsEmpty)
             {
-                throw new Exception("El arreglo esta vacio");
+                throw new Exception("The array is empty.");
             }
-            
+
             if (position < 0)
             {
                 position = 0;
@@ -73,45 +85,194 @@
             {
                 _array[i] = _array[i + 1];
             }
+
             _top--;
         }
 
-        public Vector GetOdds() 
+        public void Fill()
+        {
+            Fill(1, 100);
+        }
+
+        public void Fill(int minimun, int maximun)
+        {
+            var random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                _array[i] = random.Next(minimun, maximun);
+            }
+            _top = N;
+        }
+
+        public Vector GetPrimes()
+        {
+            int primesCount = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                if (IsPrime(_array[i]))
+                {
+                    primesCount++;
+                }
+            }
+
+            var primesArray = new Vector(primesCount);
+            for (int i = 0; i < _top; i++)
+            {
+                if (IsPrime(_array[i]))
+                {
+                    primesArray.Add(_array[i]);
+                }
+            }
+
+            return primesArray;
+
+        }
+
+        public Vector GetOdds()
         {
             int oddsCount = 0;
             for (int i = 0; i < _top; i++)
             {
-                if (_array[i]%2 == 0)
+                if (IsOdd(_array[i]))
                 {
                     oddsCount++;
                 }
             }
+
             var oddsArray = new Vector(oddsCount);
             for (int i = 0; i < _top; i++)
             {
-                if (_array[i]%2 == 0)
+                if (IsOdd(_array[i]))
                 {
                     oddsArray.Add(_array[i]);
                 }
             }
+
             return oddsArray;
         }
 
-        public void Fill(int min, int max )
+        public Vector GetNonRepeated()
         {
-            Random random = new();
-            for ( int i = 0; i < N; i++ ) 
+            int nonRepeatedCount = 0;
+            for (int i = 0; i < _top; i++)
             {
-                _array[i] = random.Next(min, max); 
+                bool isRepeated = false;
+                for (int j = 0; j < _top; j++)
+                {
+                    if (i != j && _array[i] == _array[j])
+                    {
+                        isRepeated = true;
+                        break;
+                    }
+                }
+                if (!isRepeated)
+                {
+                    nonRepeatedCount++;
+                }
             }
-            _top = N;
+
+            var nonRepeated = new Vector(nonRepeatedCount);
+            for (int i = 0; i < _top; i++)
+            {
+                bool isRepeated = false;
+                for (int j = 0; j < _top; j++)
+                {
+                    if (i != j && _array[i] == _array[j])
+                    {
+                        isRepeated = true;
+                        break;
+                    }
+                }
+                if (!isRepeated)
+                {
+                    nonRepeated.Add(_array[i]);
+                }
+            }
+            return nonRepeated;
+        }
+
+        public Vector GetMostRepeated()
+        {
+            int[,] matrixCount = new int[_top, 2];
+            int topMatrix = FillMatrixCount(matrixCount);
+            OrderMatrixCount(matrixCount, topMatrix);
+            int repeatedCounter = GetRepeatedCounter(matrixCount, topMatrix);
+            return FillMostRepeated(repeatedCounter, matrixCount);
+        }
+
+        private Vector FillMostRepeated(int repeatedCounter, int[,] matrixCount)
+        {
+            var mostRepeated = new Vector(repeatedCounter);
+            for (int i = 0; i < repeatedCounter; i++)
+            {
+                mostRepeated.Add(matrixCount[i, 0]);
+            }
+            return mostRepeated;
+        }
+
+        private int GetRepeatedCounter(int[,] matrixCount, int topMatrix)
+        {
+            int repeatedCounter = 1;
+            for (; repeatedCounter < topMatrix; repeatedCounter++)
+            {
+                if (matrixCount[0, 1] != matrixCount[repeatedCounter, 1])
+                {
+                    break;
+                }
+            }
+            return repeatedCounter;
+        }
+
+        private void OrderMatrixCount(int[,] matrixCount, int topMatrix)
+        {
+            for (int i = 0; i < topMatrix - 1; i++)
+            {
+                for (int j = i + 1; j < topMatrix; j++)
+                {
+                    if (matrixCount[i, 1] < matrixCount[j, 1])
+                    {
+                        Change(ref matrixCount[i, 0], ref matrixCount[j, 0]);
+                        Change(ref matrixCount[i, 1], ref matrixCount[j, 1]);
+                    }
+                }
+            }
+        }
+
+        private int FillMatrixCount(int[,] matrixCount)
+        {
+            int topMatrix = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                int j = 0;
+                for (; j < topMatrix; j++)
+                {
+                    if (_array[i] == matrixCount[j, 0])
+                    {
+                        matrixCount[j, 1]++;
+                        break;
+                    }
+                }
+                if (j == topMatrix)
+                {
+                    matrixCount[topMatrix, 0] = _array[i];
+                    matrixCount[topMatrix, 1] = 1;
+                    topMatrix++;
+                }
+            }
+
+            return topMatrix;
+        }
+
+        private bool IsOdd(int n)
+        {
+            return n % 2 == 0;
         }
 
         public override string ToString()
         {
             if (IsEmpty)
             {
-                return "Arreglo vacio";
+                return "Empty array.";
             }
 
             string output = string.Empty;
@@ -120,7 +281,7 @@
             {
                 output += $"{_array[i]}\t";
                 count++;
-                if(count > 9)
+                if (count > 9)
                 {
                     count = 0;
                     output += "\n";
@@ -131,39 +292,52 @@
 
         public void Sort()
         {
-            Sort(false);
+            Sort(descending: false);
         }
 
-        public void Sort(bool desc)
+        public void Sort(bool descending)
         {
             for (int i = 0; i < _top - 1; i++)
             {
                 for (int j = i + 1; j < _top; j++)
                 {
-                    if(desc)
-                    {
-                        if (_array[i] > _array[j])
-                        {
-                            Change(ref _array[i], ref _array[j]);
-                        }
-                    }
-                    else
+                    if (descending)
                     {
                         if (_array[i] < _array[j])
                         {
                             Change(ref _array[i], ref _array[j]);
                         }
                     }
-                    
+                    else
+                    {
+                        if (_array[i] > _array[j])
+                        {
+                            Change(ref _array[i], ref _array[j]);
+                        }
+                    }
                 }
             }
+        }
+
+        private bool IsPrime(int n)
+        {
+            if (n == 1) return false;
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         private void Change(ref int a, ref int b)
         {
             int aux = a;
             a = b;
-            b = aux;            
+            b = aux;
         }
     }
 }
--- Arrays/Arrays.UI.Console/Program.cs
+++ Arrays/Arrays.UI.Console/Program.cs
@@ -1,41 +1,83 @@
-﻿
+﻿using Arrays.Logic;
 
-using Arrays.Logic;
-
-try
+namespace Arrays
 {
-	Console.WriteLine("Hello Vector!");
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello, Arrays!");
+                int option;
+                var myArray = new Vector(10);
+                myArray.Fill();
+                do
+                {
+                    option = Menu();
+                    switch (option)
+                    {
+                        case 1:
+                            Console.Write("Ingrese númer de elementos: ");
+                            var nString = Console.ReadLine();
+                            int n = 10;
+                            int.TryParse(nString, out n);
+                            myArray = new Vector(n);
+                            myArray.Fill();
+                            break;
+                        case 2:
+                            Console.WriteLine(myArray);
+                            break;
+                        case 3:
+                            Console.WriteLine(myArray.GetOdds());
+                            break;
+                        case 4:
+                            Console.WriteLine(myArray.GetPrimes());
+                            break;
+                        case 5:
+                            Console.WriteLine(myArray.GetNonRepeated());
+                            break;
+                        case 6:
+                            Console.WriteLine(myArray.GetMostRepeated());
+                            break;
+                        default:
+                            Console.WriteLine("Fuck you. Opción no existe!!!!");
+                            break;
+                    }
+                } while (option != 0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
 
-	Vector vector = new(50);
-    vector.Fill(1, 100);
-    //vector.Add(10);
-    //vector.Add(20);
-    //vector.Add(30);
-    //vector.Insert(7, 0);
-    //for (int i = 0; i < vector.N; i++)
-    //{
-    //	vector.Add(i * 3);
-    //}
-    Console.WriteLine(vector);
-	//vector.Remove(0);
-	Console.WriteLine("------------------------");
-    var odds = vector.GetOdds();
-	Console.WriteLine(odds);
+        private static int Menu()
+        {
+            Console.WriteLine("1. Definir tamaño de arreglo");
+            Console.WriteLine("2. Mostrar arreglo");
+            Console.WriteLine("3. Mostrar múmeros pares");
+            Console.WriteLine("4. Mostrar múmeros primos");
+            Console.WriteLine("5. Mostrar múmeros que no se repiten");
+            Console.WriteLine("6. Mostrar múmeros que más se repiten");
+            Console.WriteLine("0. Salir");
+            bool isValid = false;
+            int option = 0;
+            do
+            {
+                Console.Write("Digite su opción: ");
+                var optionString = Console.ReadLine();
+                if (!int.TryParse(optionString, out option))
+                {
+                    Console.WriteLine("Opción inválida, solo use números.");
+                    isValid = false;
+                }
+                else
+                {
+                    isValid = true;
+                }
+            } while (!isValid);
+            return option;
+        }
+    }
 }
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
-//vector.Fill(1, 100);
-
-//Console.WriteLine("Unsorted Array!");
-//Console.WriteLine(vector);
-
-//vector.Sort(true);
-//Console.WriteLine("Ascending Sorted Array!");
-//Console.WriteLine(vector);
-
-//vector.Sort(false);
-//Console.WriteLine("Descending Sorted Array!");
-//Console.WriteLine(vector);
